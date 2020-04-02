3a1674a4c493d7d36a3ed5cade22c13a9907d64e
snazau <snazarikov@gmail.com>
2020-04-02 17:44:21 +0200

    Model update
    Activation functions choice available

--- dqn.py
+++ dqn.py
@@ -8,12 +8,15 @@ output_nodes = 3
 
 
 class DQN(nn.Module):
-    def __init__(self, hidden_size_1, hidden_size_2):
+    def __init__(self, hidden_size_1, hidden_size_2, activation="relu"):
         super(DQN, self).__init__()
         self.layer1 = nn.Linear(input_nodes, hidden_size_1)
         self.layer2 = nn.Linear(hidden_size_1, hidden_size_2)
         self.layer3 = nn.Linear(hidden_size_2, output_nodes)
-        self.activation = nn.ReLU()
+        if activation.lower() == "prelu":
+            self.activation = nn.PReLU()
+        else:
+            self.activation = nn.ReLU()
 
     def forward(self, x):
         x = self.activation(self.layer1(x))
